e1c8835c4ed377cb81a59e1c505141077db91df0
Artemon-x58 <artem1145819@gmail.com>
2023-07-21 10:32:22 +0200

    update

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Notiflix from 'notiflix';
+import Swiper from 'swiper';
+import 'swiper/css'
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+
+import './js/test';
