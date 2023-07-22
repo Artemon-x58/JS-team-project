984c284443bfe3896ef823f26e81595b4e7c27f3
Svetlana Yurikova <svetlana081294@gmail.com>
2023-07-22 20:55:15 +0300

    Squashed commit of the following:
    commit 2c5d092392ae2f78f1c9fe6994778c500a0ba43e
    Merge: b5b9b72 bafe337
    Author: Artemon-x58 <123871589+Artemon-x58@users.noreply.github.com>
    Date:   Sat Jul 22 16:48:24 2023 +0200
    
        Merge pull request #3 from Artemon-x58/pn-backend
    
        Pn backend
    
    commit bafe33720baf0c26b536c2e5a1f4f54f3f851369
    Author: pnyekrasov <pnekrasov1910@gmail.com>
    Date:   Sat Jul 22 17:25:58 2023 +0300
    
        backtnd 2
    
    commit 5f399f70756ccf26d1d7d52673ac5386d0fc6a55
    Author: pnyekrasov <pnekrasov1910@gmail.com>
    Date:   Sat Jul 22 15:01:44 2023 +0300
    
        backenl
    
    commit d3cc1f308952d9280e15c0abd4d95556c5bd5eac
    Author: pnyekrasov <pnekrasov1910@gmail.com>
    Date:   Sat Jul 22 02:09:00 2023 +0300
    
        1.2 metods

--- src/js/test.js
+++ src/js/test.js
@@ -1,1 +1,20 @@
-console.log('how are you')
+console.log('how are you');
+
+import { BackendAPI } from './tasty-backend-api';
+
+// const backendReturnData = new BackendAPI();
+// backendReturnData.recipeID = '6462a8f74c3d0ddd28897fb8';
+// backendReturnData.userRatings = {
+//   rate: 4,
+//   email: 'anton@gmail.com',
+// };
+// backendReturnData.userRatings = {
+//   name: 'Ivetta',
+//   phone: '+380000000000',
+//   email: 'test@gmail.com',
+//   comment: 'So delicious',
+// };
+// const resp = backendReturnData
+//   .passRating()
+//   .then(resp => console.log(resp.data))
+//   .catch(console.warn);
