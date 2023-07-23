9252aec1f3bb4c97083818dd652846616b425a46
KostiZin <pancostian@gmail.com>
2023-07-23 19:20:59 -0400

    Push HTML

--- src/js/kzFavoriteRecipes/favoriteRecipes.js
+++ src/js/kzFavoriteRecipes/favoriteRecipes.js
@@ -1,10 +1,11 @@
-import {createButtons} from './kzCreateButtons.js'
-import {createCard} from './kzCreateCardMarkup'
+import { createButtons } from './kzCreateButtons.js';
+import { createCard } from './kzCreateCardMarkup';
 
 const recipeCard = document.querySelector('.kz-favorite-recipes');
 const categoriesFavorite = document.querySelector('.kz-btn-favorite-recipes');
 const btnAllCategories = document.querySelector('.btn-all');
-
+const favoriteDescription = document.querySelector('.kz-favorite-description');
+const favoriteSection = document.querySelector('.kz-favorite-section');
 
 function newCard() {
   return (recipeCard.innerHTML = createCard());
@@ -12,9 +13,20 @@ function newCard() {
 
 newCard();
 
-
 function newCardCategories() {
   return (categoriesFavorite.innerHTML += createButtons());
 }
 
 newCardCategories();
+
+// function noRecipes() {
+//   return `<div><svg class="kz-favorite-icon" width="22" height="22">
+//   <use href="../img/symbol-defs.svg#icon-favorite"></use>
+// </svg><p>It appears that you haven't added any recipes to your favorites yet. To get started, you can add recipes that you like to your favorites for easier access in the future.</p></div>`;
+// }
+
+// function noFavorite() {
+//   return (favoriteSection.innerHTML = noRecipes());
+// }
+
+// console.log(noFavorite());
