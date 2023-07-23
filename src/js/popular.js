c43a640e8a5830e974e4d91981df172538bd873b
Svetlana Yurikova <svetlana081294@gmail.com>
2023-07-23 16:04:58 +0300

    add popular.js

--- src/js/popular.js
+++ src/js/popular.js
@@ -1,37 +1,52 @@
-import axios from 'axios';
-export const popularRecipList = document.querySelector('.popular-recipes-list');
+import { BackendAPI } from './tasty-backend-api'; 
 
-const BASE_URL =
-  'https://tasty-treats-backend.p.goit.global/api/recipes/popular';
+const api = new BackendAPI();
+const popularList = document.querySelector('.js-popular-list');
 
-export async function getPopularRecip() {
-  try {
-    const response = await axios.get(BASE_URL);
-    const markUp = createMarkUp(response.data);
-    popularRecipList.innerHTML = markUp;
-  } catch (error) {
-    console.error(error);
-  }
-}
+document.addEventListener("DOMContentLoaded", onReload);
 
-getPopularRecip();
-
-function createMarkUp(data) {
-  return data
-    .map(({ preview, title, description, _id }) => {
-      const MAX_LENGTH = 80;
-      let trimmedDescription =
-        description.length > MAX_LENGTH
-          ? description.substring(0, MAX_LENGTH - 3) + '...'
-          : description;
-
-      return `<li class="recip-item" id="${_id}">
-          <img class="recip-img" src="${preview}" alt="${title}" width="64"/>
-          <div class="recip-content">
-            <h3 class="recip-heading">${title}</h3>
-            <p class="recip-short-descr">${trimmedDescription}</p>
-          </div>
-        </li>`;
+function onReload(){
+  generatePopularRecipeListMarkup()
+    .then(markup => {
+      if(markup === undefined) {
+        popularList.innerHTML = '<p>No popular recipes found.</p>';
+        return;
+      }
+      updateRecipeList(markup);
     })
-    .join('');
+    .catch(onError);
 }
+
+function generatePopularRecipeListMarkup(){
+    return api.searchPopularRecipes()
+      .then(res => {
+        return res.data;
+      })
+      .then(popularRecipes =>
+        popularRecipes.reduce((markup, recipeCard) => markup + renderPopularRecipeMarkup(recipeCard), ''))
+      .catch(error => {
+        console.error("Error fetching popular recipes: ", error);
+      });
+  }
+  
+
+
+function renderPopularRecipeMarkup({title, description, _id, preview}){
+  return `<li class="recip-item" id="${_id}">
+  <img class="recip-img" src="${preview}" alt="${title}" width="64"/>
+  <div class="recip-content">
+    <h3 class="recip-heading">${title}</h3>
+    <p class="recip-short-descr">${description}</p>
+  </div>
+</li>`;
+}
+
+function updateRecipeList(markup){
+  popularList.insertAdjacentHTML('beforeend', markup);
+}
+
+function onError(error){
+  popularList.innerHTML = `<p>Error loading popular recipes: ${error.message}</p>`;
+}
+
+
