cf585d92ce50b6496f0bbcd1458908b800d6f1ba
Artemon-x58 <artem1145819@gmail.com>
2023-07-23 12:44:08 +0200

    update header

--- src/js/test.js
+++ src/js/test.js
@@ -1,20 +1,20 @@
 console.log('how are you');
 
-import { BackendAPI } from './tasty-backend-api';
+// import { BackendAPI } from './tasty-backend-api';
 
-const backendReturnData = new BackendAPI();
-backendReturnData.recipeID = '6462a8f74c3d0ddd28897fb8';
-backendReturnData.userRatings = {
-  rate: 4,
-  email: 'test@gmail.com',
-};
-backendReturnData.userOrder = {
-  name: 'Ivetta',
-  phone: '+380000000000',
-  email: 'test@gmail.com',
-  comment: 'So delicious',
-};
-const resp = backendReturnData
-  .passRating()
-  .then(resp => console.log(resp.data))
-  .catch(console.warn);
+// const backendReturnData = new BackendAPI();
+// backendReturnData.recipeID = '6462a8f74c3d0ddd28897fb8';
+// backendReturnData.userRatings = {
+//   rate: 4,
+//   email: 'test@gmail.com',
+// };
+// backendReturnData.userOrder = {
+//   name: 'Ivetta',
+//   phone: '+380000000000',
+//   email: 'test@gmail.com',
+//   comment: 'So delicious',
+// };
+// const resp = backendReturnData
+//   .passRating()
+//   .then(resp => console.log(resp.data))
+//   .catch(console.warn);
