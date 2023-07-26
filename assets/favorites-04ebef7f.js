38589b256dd3ac10f9c186333e727decee511ed1
Artemon-x58 <123871589+Artemon-x58@users.noreply.github.com>
2023-07-26 23:09:49 +0000

    Deploying to gh-pages from @ Artemon-x58/JS-team-project@76a74792bd32693b5ab5c9fb9a1d40e31b9010ed 🚀

--- assets/favorites-04ebef7f.js
+++ assets/favorites-04ebef7f.js
@@ -1,25 +1,25 @@
-import"./style-b2431025.js";const c=document.querySelector(".main-btn"),r=document.querySelector(".categories-gallery"),a=document.querySelector(".categories-btn-pn"),h=document.querySelector(".favorit-gallary-full"),S=document.querySelector("favorit-gallary-null"),u=document.querySelector(".hero-favorites");u.hidden=!1;const o=JSON.parse(localStorage.getItem("favoritiesRecipes"));r.innerHTML=v(o);r.children.length===0&&(window.innerWidth>=768&&(u.hidden=!0),u.hidden=!1,h.hidden=!0,S.hidden=!1);c.classList.add("btn-active");c.setAttribute("disabled","");c.addEventListener("click",L);function L(){a.childNodes.forEach(e=>e.classList.remove("btn-active")),c.classList.add("btn-active"),r.innerHTML="",r.innerHTML=v(o)}function v(t){return t.map(({_id:e,title:i,description:l,rating:s,preview:d,category:g})=>`<div id="${e}" data-category="${g}" class="grid-item" style="background-image: linear-gradient(1deg, rgba(5, 5, 5, 0.60) 0%, rgba(5, 5, 5, 0.00) 100%), url('${d}'); background-size: cover; background-position: center;">
+import"./mobile-menu-7a27dda3.js";const c=document.querySelector(".main-btn"),r=document.querySelector(".categories-gallery"),a=document.querySelector(".categories-btn-pn"),h=document.querySelector(".favorit-gallary-full"),S=document.querySelector("favorit-gallary-null"),u=document.querySelector(".hero-favorites");u.hidden=!1;const o=JSON.parse(localStorage.getItem("favoritiesRecipes"));r.innerHTML=v(o);r.children.length===0&&(window.innerWidth>=768&&(u.hidden=!0),u.hidden=!1,h.hidden=!0,S.hidden=!1);c.classList.add("btn-active");c.setAttribute("disabled","");c.addEventListener("click",L);function L(){a.childNodes.forEach(e=>e.classList.remove("btn-active")),c.classList.add("btn-active"),r.innerHTML="",r.innerHTML=v(o)}function v(t){return t.map(({_id:e,title:i,description:l,rating:s,preview:d,category:g})=>`<div id="${e}" data-category="${g}" class="grid-item" style="background-image: linear-gradient(1deg, rgba(5, 5, 5, 0.60) 0%, rgba(5, 5, 5, 0.00) 100%), url('${d}'); background-size: cover; background-position: center;">
         <svg class="filters-icon-heart">
-            <use href="../img/symbol-defs.svg#icon-heart"></use>
+            <use href="./img/symbol-defs.svg#icon-heart"></use>
         </svg>
         <h2 class="filters-title-recipe">${i}</h2>
         <p class="filters-description-recipe">${l}</p>
         <div class="filters-rating-wrap">
   <p class="filters-rating-recipe">${s}</p>
   <svg class="filters-icon-rating-recipe-1 ${n(s,1)}">
-    <use href="../img/symbol-defs.svg#icon-star"></use>
+    <use href="./img/symbol-defs.svg#icon-star"></use>
   </svg>
   <svg class="filters-icon-rating-recipe-2 ${n(s,2)}">
-    <use href="../img/symbol-defs.svg#icon-star"></use>
+    <use href="./img/symbol-defs.svg#icon-star"></use>
   </svg>
   <svg class="filters-icon-rating-recipe-3 ${n(s,3)}">
-    <use href="../img/symbol-defs.svg#icon-star"></use>
+    <use href="./img/symbol-defs.svg#icon-star"></use>
   </svg>
   <svg class="filters-icon-rating-recipe-4 ${n(s,4)}">
-    <use href="../img/symbol-defs.svg#icon-star"></use>
+    <use href="./img/symbol-defs.svg#icon-star"></use>
   </svg>
   <svg class="filters-icon-rating-recipe-5 ${n(s,5)}">
-    <use href="../img/symbol-defs.svg#icon-star"></use>
+    <use href="./img/symbol-defs.svg#icon-star"></use>
   </svg>
   <button class="filters-btn-recipe" type="button">See recipe</button>
 </div>
