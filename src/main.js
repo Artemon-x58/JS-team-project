970ccf134082a27a7c843ee4217d813a35a93384
Artemon-x58 <artem1145819@gmail.com>
2023-07-26 10:17:19 +0200

    s

--- src/main.js
+++ src/main.js
@@ -9,12 +9,12 @@ import './js/tasty-backend-api';
 import './js/mobile-menu';
 import './js/popular';
 import './js/modal-rating';
-// import './js/hero-home';
+import './js/hero-home';
 import './js/categories';
 
 import './js/modal-order';
 import './js/filters';
-import './js/filters';
+
 
 
 
