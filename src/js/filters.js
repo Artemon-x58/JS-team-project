14d092b6deace6beeb009c42e4466a8d90a818e9
Artemon-x58 <artem1145819@gmail.com>
2023-07-27 15:40:02 +0200

    Update filters.js

--- src/js/filters.js
+++ src/js/filters.js
@@ -318,7 +318,7 @@ function createRecipeContainers(numContainers, _id, title, description, rating, 
     for (let i = 0; i < numContainers; i++) {
         
     const linkStar = "../img/symbol-defs.svg#icon-star";
-    const linkHeart = "../img/symbol-defs.svg#icon-heart"
+    const linkHeart = "../img/symbol-defs.svg#"
     const murkup = `<div id="${_id}" data-category="${category}" class="filters-box-child" style="background-image: linear-gradient(1deg, rgba(5, 5, 5, 0.60) 0%, rgba(5, 5, 5, 0.00) 100%), url('${preview}'); background-size: cover; background-position: center;">
         <svg class="filters-icon-heart">
             <use href="${linkHeart}icon-heart"></use>
